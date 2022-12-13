645caeebe7efa9173f8022f3f14e79cb3730c41d
ndunnett <46371520+ndunnett@users.noreply.github.com>
2022-12-13 08:23:28 +0000

    more optimisation

--- source/day11.py
+++ source/day11.py
@@ -1,12 +1,12 @@
+from copy import deepcopy
 import re
 from functools import reduce
 from input import load_input
 
 
 class Monkey:
-    def __init__(self, string, relief):
+    def __init__(self, string):
         self.items = [int(match.strip()) for match in re.search(r"(?:Starting items:)(.*)", string).group(1).split(",")]
-        self.relief = relief
         self.operation = lambda _: _
         exec("self.operation = lambda old: " + re.search(r"(?:Operation: new = )(.*)", string).group(1).strip())
         self.test = int(re.search(r"(?:Test: divisible by)(.*)", string).group(1))
@@ -17,15 +17,15 @@ class Monkey:
 
 def puzzle():
     lines = load_input(11)
-    monkeys = [Monkey(match.strip(), 3) for match in re.split(r"Monkey [\d]+:", "".join(lines)) if match]
-    modulus = reduce(lambda x, y: x * y, [monkey.relief * monkey.test for monkey in monkeys])
+    monkeys_orig = [Monkey(match.strip()) for match in re.split(r"Monkey [\d]+:", "".join(lines)) if match]
+    modulus = reduce(lambda x, y: x * y, [monkey.test for monkey in monkeys_orig])
+    monkeys = deepcopy(monkeys_orig)
 
     for _ in range(20):
         for monkey in monkeys:
             for item in monkey.items:
-                worry = monkey.operation(item) // monkey.relief % modulus
-                throw_to = monkey.true if worry % monkey.test == 0 else monkey.false
-                monkeys[throw_to].items.append(worry)
+                worry = monkey.operation(item) // 3
+                monkeys[monkey.true if worry % monkey.test == 0 else monkey.false].items.append(worry)
             monkey.inspections += len(monkey.items)
             monkey.items.clear()
 
@@ -33,22 +33,19 @@ def puzzle():
     answer = inspections[0] * inspections[1]
     print(f"Part 1 answer: {answer}")
 
-    monkeys = [Monkey(match.strip(), 1) for match in re.split(r"Monkey [\d]+:", "".join(lines)) if match]
-    modulus = reduce(lambda x, y: x * y, [monkey.relief * monkey.test for monkey in monkeys])
+    monkeys = deepcopy(monkeys_orig)
 
     for _ in range(10000):
         for monkey in monkeys:
             for item in monkey.items:
-                worry = monkey.operation(item) // monkey.relief % modulus
-                throw_to = monkey.true if worry % monkey.test == 0 else monkey.false
-                monkeys[throw_to].items.append(worry)
+                worry = monkey.operation(item) % modulus
+                monkeys[monkey.true if worry % monkey.test == 0 else monkey.false].items.append(worry)
             monkey.inspections += len(monkey.items)
             monkey.items.clear()
 
     inspections = sorted([monkey.inspections for monkey in monkeys], reverse=True)
     answer = inspections[0] * inspections[1]
     print(f"Part 2 answer: {answer}")
-
 
 
 if __name__ == "__main__":
