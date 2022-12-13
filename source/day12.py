15fea036956431f8bb485938b255586a7c9fea99
ndunnett <46371520+ndunnett@users.noreply.github.com>
2022-12-13 14:48:40 +0000

    day 12

--- source/day12.py
+++ source/day12.py
@@ -1,92 +1,82 @@
-# from input import load_input
+from copy import deepcopy
+from functools import reduce
+from input import load_input
+
+
+class Node:
+    def __init__(self, x, y, char):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.dist = 10**10
+        self.visited = False
+
+        if char == "E":
+            self.value = ord("z") - ord("a")
+        elif char == "S":
+            self.value = ord("z") - ord("a")
+        else:
+            self.value = ord(char) - ord("a")
 
 
 def puzzle():
-    # lines = load_input(12)
+    lines = load_input(12)
+    nodes = reduce(lambda a, b: a + b, [[Node(x, y, char) for x, char in enumerate(line)] for y, line in enumerate(lines)], [])
 
-    lines = """Sabqponm
-abcryxxl
-accszExk
-acctuvwj
-abdefghi""".splitlines()
+    def index_in_limits(index):
+        return index[0] >= 0 and index[0] < len(lines[0]) and index[1] >= 0 and index[1] < len(lines)
 
-    def back_track(moves, x, y):
-        current = ord(lines[y][x])
+    def next_node(nodes):
+        min_dist = 10**10
+        min_node = None
 
-        if current == ord("E"):
-            return moves
+        for node in nodes:
+            if node.dist < min_dist and not node.visited:
+                min_dist = node.dist
+                min_node = node
 
-        if current == ord("S"):
-            current = 999
+        return min_node
 
-        candidates = []
+    def dijkstra(nodes, start_char, target_char):
+        paths = []
 
-        if y > 0:
-            index = (x, y - 1)
-            value = ord(lines[index[1]][index[0]])
+        for _node in nodes:
+            if _node.char == start_char:
+                for __node in nodes:
+                    __node.dist = 10**10
+                    __node.visited = False
 
-            if value == ord("E"):
-                value = ord("z")
+                node = _node
+                target = None
 
-            if value <= current + 1 and index not in moves:
-                candidates += [index]
+                for _node in nodes:
+                    if _node.char == target_char:
+                        target = _node
 
-        if y < len(lines) - 1:
-            index = (x, y + 1)
-            value = ord(lines[index[1]][index[0]])
+                node.dist = 0
 
-            if value == ord("E"):
-                value = ord("z")
+                for _ in range(len(nodes)):
+                    node.visited = True
+                    indices = [index for index in [(node.x, node.y - 1), (node.x, node.y + 1), (node.x - 1, node.y), (node.x + 1, node.y)] if index_in_limits(index)]
+                    neighbours = [neighbour for neighbour in [nodes[index[0] + index[1] * len(lines[0])] for index in indices] if neighbour.value <= node.value + 1]
 
-            if value <= current + 1 and index not in moves:
-                candidates += [index]
+                    for neighbour in neighbours:
+                        if not neighbour.visited and node.dist + 1 < neighbour.dist:
+                            neighbour.dist = node.dist + 1
 
-        if x > 0:
-            index = (x - 1, y)
-            value = ord(lines[index[1]][index[0]])
+                    node = next_node(nodes)
 
-            if value == ord("E"):
-                value = ord("z")
+                    if not node or target.visited:
+                        break
 
-            if value <= current + 1 and index not in moves:
-                candidates += [index]
+                paths += [target.dist]
 
-        if x < len(lines[0]) - 1:
-            index = (x + 1, y)
-            value = ord(lines[index[1]][index[0]])
+        return min(paths)
 
-            if value == ord("E"):
-                value = ord("z")
+    answer = dijkstra(deepcopy(nodes), "S", "E")
+    print(f"Part 1 answer: {answer}")
 
-            if value <= current + 1 and index not in moves:
-                candidates += [index]
-
-        valid_paths = []
-
-        for candidate in candidates:
-            path = back_track(moves + [candidate], candidate[0], candidate[1])
-
-            if path:
-                valid_paths += [path]
-
-        if valid_paths:
-            return sorted(valid_paths, reverse=True)[0]
-
-        return None
-
-    current_x = 0
-    current_y = 0
-
-    for y, line in enumerate(lines):
-        for x, char in enumerate(line):
-            if char == "S":
-                current_x = x
-                current_y = y
-
-    answer = back_track([(current_x, current_y)], current_x, current_y)
-    print(f"Part 1 answer: {len(answer[1:])}")
-
-    answer = 0
+    answer = dijkstra(deepcopy(nodes), "a", "E")
     print(f"Part 2 answer: {answer}")
 
 
